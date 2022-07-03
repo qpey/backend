9cfa701fc4a55561a559fca6f362f7167dc6f6c1
ianbalijawa <ianbalijawa16@gmail.com>
2022-07-04 00:01:05 +0300

    change:add redis cache layer but still not fully set-up

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { Express } from 'express';
+import { Request, Response } from 'express';
+import { NotAuthorizedError } from './errors';
+import { errorHandler } from './middlewares';
+
+const app: Express = express();
+
+app.set('trust proxy', true);
+
+app.all('*', async (req: Request, res: Response): Promise<never> => {
+	throw new NotAuthorizedError('Route to resource not Found');
+});
+
+app.get('/', (req: Request, res: Response) => res.send('Hello there !'));
+
+app.use(errorHandler);
+
+export { app };
