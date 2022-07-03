4ffc415f847f4e22470e4e2d05d78f05f11d2b4c
ianbalijawa <ianbalijawa16@gmail.com>
2022-07-03 16:55:45 +0300

    fix: fixed imports

--- src/errors/request-validation-error.ts
+++ src/errors/request-validation-error.ts
@@ -1,9 +1,10 @@
 import { CustomError, ErrorFields } from './custom-error';
+import {ValidationError} from "express-validator";
 
+// TODO: implememt this ValidationError type specific to (joi)
 export class RequestValidationError extends CustomError {
     statusCode = 400;
 
-    // TODO: implememt this ValidationError type specific to (joi)
     constructor(private errors: ValidationError[]) {
         super('Invalid Request Parameters');
 
@@ -12,7 +13,7 @@ export class RequestValidationError extends CustomError {
 
     serializeErrors(): ErrorFields[] {
         return this.errors.map(error => {
-            return {message:error.message, field:error.param}
+            return {message:error.msg, field:error.param}
         })
     }
 }
